203f303675512e29710049c43b06f866c125054c
Nisha Kuvalekar <121301819+nishakuvalekar25@users.noreply.github.com>
2023-12-16 11:09:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-link");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab")
+}
+
